bfbb49243a8ae0ff9106010d0ebb2b3248421548
Anthony Gauthier <antho325@hotmail.com>
2024-07-12 09:22:35 -0400

    docs: added examples for multi rules definition in group_alerting and group_recording
    Signed-off-by: Anthony Gauthier <antho325@hotmail.com>

--- examples/resources/loki_rule_group_alerting/resource.tf
+++ examples/resources/loki_rule_group_alerting/resource.tf
@@ -8,6 +8,25 @@ sum(rate({app="foo", env="production"} |= "error" [5m])) by (job)
   /
 sum(rate({app="foo", env="production"}[5m])) by (job)
   > 0.05
+EOT
+    for         = "10m"
+    labels      = {
+      severity = "warning"
+    }
+    annotations = {
+      summary = "High request latency"
+    }
+  }
+
+  # can define multiple rules
+  rule {
+    alert       = "HighPercentageError"
+    expr        = <<EOT
+sum(rate({app="bar", env="dev"} |= "error" [5m])) by (job)
+  /
+sum(rate({app="bar", env="dev"}[5m])) by (job)
+  > 0.05
+EOT
     for         = "10m"
     labels      = {
       severity = "warning"

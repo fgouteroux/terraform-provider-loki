8741f503edbb53810fc9092e9fbc3c8d6c75c8fa
François Gouteroux <francois.gouteroux@gmail.com>
2025-09-30 12:19:47 +0200

    ci: fix TestAccDataSourceRuleGroup_list random fail

--- loki/data_source_loki_rule_group_list_test.go
+++ loki/data_source_loki_rule_group_list_test.go
@@ -1,9 +1,11 @@
 package loki
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDataSourceRuleGroup_list(t *testing.T) {
@@ -14,22 +16,83 @@ func TestAccDataSourceRuleGroup_list(t *testing.T) {
 			{
 				Config: testAccDataSourceRuleGroup_list,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.namespace", "namespace_1"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.rule_groups.0.name", "alert_1"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.rule_groups.0.rule.0.alert", "test1"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.namespace", "namespace_1"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.rule_groups.1.name", "alert_2"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.0.rule_groups.1.rule.0.alert", "test2"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.namespace", "namespace_2"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.rule_groups.0.name", "alert_3"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.rule_groups.0.rule.0.alert", "test3"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.namespace", "namespace_2"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.rule_groups.1.name", "record_1"),
-					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.1.rule_groups.1.rule.0.record", "nginx:requests:rate1m"),
+					// Check that we have 2 namespaces
+					resource.TestCheckResourceAttr("data.loki_rule_group_list.all", "namespaces.#", "2"),
+
+					// Use custom function to check namespace_1 and its contents
+					testAccCheckNamespaceExists("data.loki_rule_group_list.all", "namespace_1", []string{"alert_1", "alert_2"}),
+
+					// Use custom function to check namespace_2 and its contents
+					testAccCheckNamespaceExists("data.loki_rule_group_list.all", "namespace_2", []string{"alert_3", "record_1"}),
 				),
 			},
 		},
 	})
+}
+
+// testAccCheckNamespaceExists checks if a namespace exists with expected rule groups
+func testAccCheckNamespaceExists(dataSourceName, namespaceName string, expectedGroups []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[dataSourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", dataSourceName)
+		}
+
+		// Find the namespace in the list
+		namespaceCount := rs.Primary.Attributes["namespaces.#"]
+		if namespaceCount == "" {
+			return fmt.Errorf("No namespaces found")
+		}
+
+		// Search through all namespaces to find the one we're looking for
+		var foundNamespace bool
+		var namespaceIndex string
+
+		for i := 0; ; i++ {
+			nsKey := fmt.Sprintf("namespaces.%d.namespace", i)
+			ns, exists := rs.Primary.Attributes[nsKey]
+			if !exists {
+				break
+			}
+			if ns == namespaceName {
+				foundNamespace = true
+				namespaceIndex = fmt.Sprintf("%d", i)
+				break
+			}
+		}
+
+		if !foundNamespace {
+			return fmt.Errorf("Namespace %s not found in data source", namespaceName)
+		}
+
+		// Check rule groups count
+		ruleGroupsKey := fmt.Sprintf("namespaces.%s.rule_groups.#", namespaceIndex)
+		ruleGroupsCount := rs.Primary.Attributes[ruleGroupsKey]
+		if ruleGroupsCount != fmt.Sprintf("%d", len(expectedGroups)) {
+			return fmt.Errorf("Expected %d rule groups in namespace %s, got %s",
+				len(expectedGroups), namespaceName, ruleGroupsCount)
+		}
+
+		// Check that all expected rule groups exist (order-independent)
+		foundGroups := make(map[string]bool)
+		for i := 0; ; i++ {
+			nameKey := fmt.Sprintf("namespaces.%s.rule_groups.%d.name", namespaceIndex, i)
+			name, exists := rs.Primary.Attributes[nameKey]
+			if !exists {
+				break
+			}
+			foundGroups[name] = true
+		}
+
+		for _, expected := range expectedGroups {
+			if !foundGroups[expected] {
+				return fmt.Errorf("Expected rule group %s not found in namespace %s",
+					expected, namespaceName)
+			}
+		}
+
+		return nil
+	}
 }
 
 var testAccDataSourceRuleGroup_list = `
@@ -70,6 +133,12 @@ var testAccDataSourceRuleGroup_list = `
 	}
 
 	data "loki_rule_group_list" "all" {
-		name = loki_rule_group_alerting.alert_3.name
+	    depends_on = [
+	    	loki_rule_group_alerting.alert_1,
+	    	loki_rule_group_alerting.alert_2,
+	    	loki_rule_group_alerting.alert_3,
+	    	loki_rule_group_recording.record_1,
+	  	]
+	  name = "test"
 	}
 `
